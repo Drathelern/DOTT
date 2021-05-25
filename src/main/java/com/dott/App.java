698bb7bae5a965e92fa86c8c9656502c53f830e7
Drathelern <67207903+Drathelern@users.noreply.github.com>
2021-05-25 12:12:45 -0500

    logger.log

--- src/main/java/com/dott/App.java
+++ src/main/java/com/dott/App.java
@@ -6,7 +6,7 @@ import static spark.Spark.*;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println( "Listening on: http://localhost:8000/" );
+        logger.log( "Listening on: http://localhost:8000/" );
 
         port(8000);
         get("/", App::routeRoot);
